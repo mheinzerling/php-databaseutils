cb326698e7b869a8e1ffc8618c5d986d4f180654
Martin Heinzerling <webmaster@mheinzerling.de>
2013-08-02 18:43:35 +0200

    execute sql file

--- src/mheinzerling/commons/database/DatabaseUtils.php
+++ src/mheinzerling/commons/database/DatabaseUtils.php
@@ -41,4 +41,9 @@ class DatabaseUtils
         }
         return $result;
     }
+	
+	public static function executeFile(\PDO $connection, $sqlFileName)
+    {
+        $connection->exec(file_get_contents($sqlFileName));
+    }
 }
