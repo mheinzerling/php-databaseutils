8e78c8d0bc143b18d46698e2b866dfa8912a7be6
Martin Heinzerling <webmaster@mheinzerling.de>
2014-01-26 17:22:31 +0100

    move PersistenceProvider from entity to databaseutils

---
+++ src/mheinzerling/commons/database/PersistenceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace mheinzerling\commons\database;
+
+
+class PersistenceProvider
+{
+	/**
+	 * @var \PDO
+	 */
+	private static $connection;
+
+	/**
+	 * @return \PDO
+	 * @throws \Exception
+	 */
+	public static function getConnection()
+    {
+        if (self::$connection == null) throw new \Exception("No default connection defined. Call PersistenceManager::setConnection"); //TODO
+        return self::$connection;
+    }
+
+    public static function setConnection(\PDO $connection)
+    {
+        self::$connection = $connection;
+        //TODO listener
+    }
+
+    public static function resetConnection()
+    {
+        self::$connection = null;
+        //TODO listener
+    }
+}
